5dd1625580b28cb199cf0a2e0e3beca5d2295b6a
Amadeusz Pęcherczyk <apecherczyk@protonmail.com>
2021-09-12 00:45:49 +0200

    Poprawienie wyświetlania wyników wyszukiwania.

--- src/SearchTab/SearchResults.js
+++ src/SearchTab/SearchResults.js
@@ -3,11 +3,18 @@ import React from 'react';
 export default class SearchResult extends React.Component {
   render () {
     return (
-      <div className='row border-bottom'>
-        <div className='col-12 py-3'>
-          <div className='fs-5' onClick={() => this.props.onSearchResultClick(this.props.searchResult._id)} role='button'>{this.props.searchResult.title}</div>
-          {this.props.searchResult.albumName && <span className='text-secondary me-3'>{this.props.searchResult.albumName}</span>}
-          {this.props.searchResult.artistName && <span className='text-secondary'>{this.props.searchResult.artistName}</span>}
+      <div className='row'>
+        <div className='col-12'>
+          <div className='border-bottom py-3'>
+            <div
+              className='fs-5' role='button'
+              onClick={() => this.props.onSearchResultClick(this.props.searchResult._id)}
+            >
+              {this.props.searchResult.title}
+              {this.props.searchResult.albumName && <span className='text-secondary fs-6 ms-2'>{this.props.searchResult.albumName}</span>}
+            </div>
+            {this.props.searchResult.artistName && <span className='text-secondary'>{this.props.searchResult.artistName}</span>}
+          </div>
         </div>
       </div>
     );
