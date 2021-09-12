80858e7ca9920b20c74de1fe431cf881faef70b5
Amadeusz Pęcherczyk <apecherczyk@protonmail.com>
2021-09-12 17:44:34 +0200

    Poprawa warningów w Navbar.

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,19 +6,19 @@ export default class Navbar extends React.Component {
     return (
       <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
         <div className='container-fluid'>
-          <a className='navbar-brand' href='#' onClick={() => this.props.onNavItemClick(NavBarState.home)}>TracksAbout</a>
-          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+          <span className='navbar-brand' role='button' onClick={() => this.props.onNavItemClick(NavBarState.home)}>TracksAbout</span>
+          <button
+            className='navbar-toggler' type='button'
+            data-bs-toggle='collapse' data-bs-target='#navbarNav'
+            aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'
+          >
             <span className='navbar-toggler-icon' />
           </button>
           <div className='collapse navbar-collapse' id='navbarNav'>
-            <ul className='navbar-nav'>
-              <li className='nav-item'>
-                <a className='nav-link' href='#' onClick={() => this.props.onNavItemClick(NavBarState.search)}>Search</a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='#' onClick={() => this.props.onNavItemClick(NavBarState.upload)}>Upload</a>
-              </li>
-            </ul>
+            <div className='navbar-nav'>
+              <span className='nav-link' role='button' onClick={() => this.props.onNavItemClick(NavBarState.search)}>Search</span>
+              <span className='nav-link' role='button' onClick={() => this.props.onNavItemClick(NavBarState.upload)}>Upload</span>
+            </div>
           </div>
         </div>
       </nav>
